7700ca23f1b2294de1a3fd3ff5c95851d268c31e
taisei <64033402+taisei@users.noreply.github.com>
2020-05-27 15:06:24 +0900

    [finish]4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  def top
+  end
+  def index
+      @books = Book.all
+      @book = Book.new
+  end
+  def destroy 
+        book = Book.find(params[:id])
+        book.destroy
+        redirect_to books_path
+    end
+  def show
+      @book = Book.find(params[:id])
+  end
+
+  def create
+  	  book = Book.new(book_params)
+      if book.save
+      redirect_to book_path(book), notice: 'successfully'
+      else
+       @books = Book.all
+       @book = book
+       render :index
+      end
+
+
+
+  end
+  def edit
+      @book = Book.find(params[:id])
+  end
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+    redirect_to book_path(book), notice: 'successfully'
+    else
+       @book = book
+       render :edit
+    end
+
+end
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
